feddbf0b74ed4a41240a96ef455af5f803c11d61
Deepak Lohani <deepak.lohani@skillrary.com>
2021-05-22 19:18:56 +0530

    jenkins-php-unit-project

---
+++ tests/GumballMachineTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+use deepak\GumballMachine;
+use PHPUnit\Framework\TestCase;
+
+class GumballMachineTest extends TestCase
+{
+    public $gumballMachineInstance;
+
+    public function setUp()
+    {
+        $this->gumballMachineInstance = new GumballMachine();
+    }
+
+    public function testIfWheelWorks(){
+    	// Suppose we have 100 gumballs...
+    	$this->gumballMachineInstance->setGumballs(100);
+
+    	// ... And we turn the wheel once...
+    	$this->gumballMachineInstance->turnWheel();
+
+    	// ... we should now have 99 gumballs remaining in the machine right?
+    	$this->assertEquals(99, $this->gumballMachineInstance->getGumballs()); 
+		
+    }
+}
